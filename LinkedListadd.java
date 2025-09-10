6050dd1d0e65fc5a8c9fb0af12134f9f6bd9d6c8
MO RIJVAN <morijvan271@gmail.com>
2025-09-10 11:17:22 +0530

    Create LinkedListadd.java

---
+++ LinkedListadd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Test {
+     public static void main(String args[]) {
+        LinkedList<Integer> number = new LinkedList<>();
+
+        number.add(20);
+        number.add(30);
+        number.add(40);
+
+        System.out.println("LinkedList : "+ number);
+
+    }
+}
