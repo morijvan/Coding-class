3fb828ca5cc1c1285c13d2269606650eb3c40340
MO RIJVAN <morijvan271@gmail.com>
2025-09-10 11:53:31 +0530

    Create LinkedListRemoveupdate.java

---
+++ LinkedListRemoveupdate.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Test {
+     public static void main(String args[]) {
+        LinkedList<Integer> number = new LinkedList<>();
+         //add elements in LenkedList
+        number.add(20);
+        number.add(30);
+        number.add(40);
+        number.add(50);
+        System.out.println("LinkedList : "+ number);
+
+        //remove elements from index 
+        int rem = number.remove(2);
+        System.out.println("Remove Element : "+ rem);
+        System.out.println("Update LinkedList : "+number );
+  
+
+    }
+}
