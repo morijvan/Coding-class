f266c01e93f6dce3da6b9ef32981136c5056e758
morijvan <morijvan271@gmail.com>
2025-10-08 20:57:02 +0530

    currect Function name

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,6 +1,6 @@
  import java.util.*;
  public class LinearSearch {
-    public static int LinearSearch(int numbers[], int key) {
+    public static int LinearSearchb(int numbers[], int key) {
         for(int i=0; i<numbers.length; i++) {
             if(numbers[i] == key) {
                 return i;
@@ -11,7 +11,7 @@
     public static void main(String args[]) {
         int numbers[] = {2, 5, 3, 1, 8, 7, 6, 9};
         int key = 8;
-        int index = LinearSearch(numbers, key);
+        int index = LinearSearchb(numbers, key);
         if(index == -1) { 
             System.out.println("not found");
 
